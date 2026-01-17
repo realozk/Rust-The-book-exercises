86fcbadbd2d47afe439fdf3da919c2b316ea263e
omar <omarzhrani210@gmail.com>
2026-01-17 20:18:38 +0300

    1

---
+++ cyber_watch/target/debug/deps/cyber_watch.d
@@ -0,0 +1,5 @@
+C:\Users\omar\Desktop\rust_hd\cyber_watch\target\debug\deps\cyber_watch.d: src\main.rs
+
+C:\Users\omar\Desktop\rust_hd\cyber_watch\target\debug\deps\cyber_watch.exe: src\main.rs
+
+src\main.rs:
---
+++ cyber_watch/target/debug/deps/windows_targets-9dd3e7c41f21dc7c.d
@@ -0,0 +1,7 @@
+C:\Users\omar\Desktop\rust_hd\cyber_watch\target\debug\deps\windows_targets-9dd3e7c41f21dc7c.d: C:\Users\omar\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\windows-targets-0.52.6\src\lib.rs
+
+C:\Users\omar\Desktop\rust_hd\cyber_watch\target\debug\deps\libwindows_targets-9dd3e7c41f21dc7c.rlib: C:\Users\omar\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\windows-targets-0.52.6\src\lib.rs
+
+C:\Users\omar\Desktop\rust_hd\cyber_watch\target\debug\deps\libwindows_targets-9dd3e7c41f21dc7c.rmeta: C:\Users\omar\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\windows-targets-0.52.6\src\lib.rs
+
+C:\Users\omar\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\windows-targets-0.52.6\src\lib.rs:
